7f97ebc041c4ab85a2bc3b2b782775825fa94377
Max Mackie <maxmackie13@gmail.com>
2022-03-11 18:21:07 +0000

    add Tile class

--- script.js
+++ script.js
@@ -3,3 +3,6 @@
 const gameBoard = document.getElementById("game-board")
 
 const grid = new Grid(gameBoard);
+grid.randomEmptyCell().tile = new Tile(gameBoard)
+grid.randomEmptyCell().tile = new Tile(gameBoard)
+
