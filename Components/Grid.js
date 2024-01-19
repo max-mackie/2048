cdbbec0bdcad5bd84013f078e8a87933ee98214f
Max Mackie <maxmackie13@gmail.com>
2024-01-19 16:10:25 +0000

    fix:: removed typo from css

--- Components/Grid.js
+++ Components/Grid.js
@@ -1,6 +1,7 @@
 const GRID_SIZE = 4;
-const CELL_SIZE = 20;
+const CELL_SIZE = 17;
 const CELL_GAP = 2;
+const Score = 12;
 
 
 export default class Grid {
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // https://www.youtube.com/watch?v=wOVEe9eawXc&t=1136s
 
-import Grid from "./Grid.js"
-import Tile from "./Tile.js"
+import Grid from "./Components/Grid.js"
+import Tile from "./Components/Tile.js"
 
 const gameBoard = document.getElementById("game-board")
 
